d825ced114980d0716a3483b616e3cc2bb7b594d
David <70294884+David-Woroniuk@users.noreply.github.com>
2020-10-14 11:06:14 +0100

    Update autocorrelation_test.py

--- autocorrelation_test.py
+++ autocorrelation_test.py
@@ -1,23 +1,32 @@
+import pandas as pd 
 import statsmodels.api as sm
 
-
-
-
-def autocorrelation_and_partial_test(data, lags):
+def autocorrelation_and_partial_test(data_series, lags):
   '''
   This function conducts ACF and PACF tests using the statsmodels package.
 
-  Inputs:   data: the data series to be tested (Pandas Series).
+  Inputs:   data_series: the data series to be tested (Pandas Series).
             lags: the number of lags to be tested (integer).
 
   Outputs:  N/A.
   '''
+  if isinstance(data_series, pd.Series) is False:
+      raise TypeError("data_series argument must be a Pandas Series, data['Series'].")
+  
+  if isinstance(lags, int) is False:
+    raise TypeError("lags argument must be an integer.")
+
   # print ACF and PACF stats:
   print('*'*100)
-  print('Autocorrelation structure:' , sm.graphics.tsa.acf(data, nlags = lags))
-  print('Partial Autocorrelation structure:' , sm.graphics.tsa.pacf(data, nlags = lags))
+  print('Autocorrelation structure:' , sm.graphics.tsa.acf(data_series, nlags = lags))
+  print('*'*100)
+  print('Partial Autocorrelation structure:' , sm.graphics.tsa.pacf(data_series, nlags = lags))
   print('*'*100)
 
   # plot ACF and PACF:
-  sm.graphics.tsa.plot_acf(data, lags = lags).show()
-  sm.graphics.tsa.plot_pacf(data, lags= lags).show()
+  sm.graphics.tsa.plot_acf(data_series, lags = lags).show()
+  sm.graphics.tsa.plot_pacf(data_series, lags= lags).show()
+
+
+
+autocorrelation_and_partial_test(dataset['Close'], 40)
