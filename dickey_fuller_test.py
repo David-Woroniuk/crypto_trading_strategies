d926e233dcb079656537606645339c302f3a799a
David <70294884+David-Woroniuk@users.noreply.github.com>
2020-10-14 11:07:26 +0100

    Update dickey_fuller_test.py

--- dickey_fuller_test.py
+++ dickey_fuller_test.py
@@ -1,21 +1,33 @@
-def dickey_fuller(series, regression_type, criterion):
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+
+def augmented_dickey_fuller(data_series, regression_type, criterion):
   '''
   This function conducts the augmented dickey fuller test.
-
-  Inputs:   series: This can be a Pandas DataFrame column, or Pandas Series to be tested.
+  Inputs:   data_series: the data series to be tested (Pandas Series).
             regression_type: “c” (constant only),”ct” (constant and trend),”ctt” (constant, linear & quadratic trend),”nc” (no constant or trend).
             criterion: "BIC" or "AIC" - Bayesian or Akaike Information Criteria.
 
   Prints:   ADF Statistic: test statistic.
             p-value: associated p-value.
-            critical values: confidence levels at which to accept or reject null.
-            
-  Outputs:  result[0]: ADF Statistic.
-            values_list: Critical Values at the 1%, 5% and 10% values.
+            critical values: confidence levels at which to accept or reject null.   
   '''
 
+  if isinstance(data_series, pd.Series) is False:
+    raise TypeError("data_series argument must be a Pandas Series, data['Series'].")
+  
+  if isinstance(regression_type, str) is False:
+    raise TypeError("regression_type argument must be a string.")
+  while regression_type not in ['c', 'ct', 'ctt', 'nc']:
+    raise TypeError("regression_type argument must be equal to 'c', 'ct','ctt' or 'nc'.")
+
+  if isinstance(criterion, str) is False:
+    raise TypeError("criterion argument must be a string.")
+  while criterion not in ['BIC', 'AIC']:
+    raise TypeError("criterion argument must be equal to 'BIC' or 'AIC'.")
+  
   # set X as values of input series, input conditions into statsmodels adf test:
-  X = series.values
+  X = data_series.values
   result = adfuller(X, regression = regression_type, autolag = criterion)
 
   # print outcomes:
@@ -43,4 +55,6 @@ def dickey_fuller(series, regression_type, criterion):
     print("Insignificant, fail to reject null hypothesis of unit root")
   print('*' * 100)
 
-  return result[0], values_list
+
+
+augmented_dickey_fuller(dataset['Open'], 'c','BIC')
