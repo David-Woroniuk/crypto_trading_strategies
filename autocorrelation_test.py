6b3f26e1795e67dc6ecdf87f4fec05fce28ff54c
David Woroniuk <david.woroniuk@gmail.com>
2022-09-22 17:35:29 +0100

    Modified Autocorrelation Test

--- autocorrelation_test.py
+++ autocorrelation_test.py
@@ -1,32 +1,40 @@
-import pandas as pd 
+import pandas as pd
 import statsmodels.api as sm
 
-def autocorrelation_and_partial_test(data_series, lags):
-  '''
-  This function conducts ACF and PACF tests using the statsmodels package.
 
-  Inputs:   data_series: the data series to be tested (Pandas Series).
-            lags: the number of lags to be tested (integer).
+def autocorrelation_and_partial_test(data_series: pd.Series, lags: int, verbose: bool = True):
+    """
+    This function creates the autocorrelation and partial autocorrelation tests (ACF / PACF).
+    The function firstly checks the input arguments are of the correct type, followed by
+    calculating the ACF and PACF, printing the autocorrelation structure if verbose, and
+    displaying the plots.
 
-  Outputs:  N/A.
-  '''
-  if isinstance(data_series, pd.Series) is False:
-      raise TypeError("data_series argument must be a Pandas Series, data['Series'].")
-  
-  if isinstance(lags, int) is False:
-    raise TypeError("lags argument must be an integer.")
+    :param data_series: the series on which to test the ACF and PACF. (pd.Series)
+    :param lags: the number of lags to be used. (int)
+    :param verbose: determines if the print statements are printed. (bool)
+    """
+    if not isinstance(data_series, pd.Series):
+        raise TypeError("data_series argument must be a Pandas Series, data['Series'].")
+    if not isinstance(lags, int):
+        raise TypeError("lags argument must be an integer.")
 
-  # print ACF and PACF stats:
-  print('*'*100)
-  print('Autocorrelation structure:' , sm.graphics.tsa.acf(data_series, nlags = lags))
-  print('*'*100)
-  print('Partial Autocorrelation structure:' , sm.graphics.tsa.pacf(data_series, nlags = lags))
-  print('*'*100)
+    if verbose:
+        print('*' * 100)
+        print('Auto Correlation Structure: {}'.format(sm.graphics.tsa.acf(data_series, nlags=lags)))
+        print('*' * 100)
+        print('Partial Auto Correlation Structure: {}'.format(sm.graphics.tsa.pacf(data_series, nlags=lags)))
+        print('*' * 100)
 
-  # plot ACF and PACF:
-  sm.graphics.tsa.plot_acf(data_series, lags = lags).show()
-  sm.graphics.tsa.plot_pacf(data_series, lags= lags).show()
-
+    sm.graphics.tsa.plot_acf(data_series, lags=lags).show()
+    sm.graphics.tsa.plot_pacf(data_series, lags=lags).show()
 
 
-autocorrelation_and_partial_test(dataset['Close'], 40)
+if __name__ == "__main__":
+    dataset = pd.DataFrame(
+        {
+            "ticker": ["A", "B", "C", "D", "E"],
+            "mktcap": [100, 400, 399, 120, 30],
+            "volatility": [0.3, 0.8, 0.02, 0, 1],
+        }
+    )
+  autocorrelation_and_partial_test(dataset['mktcap'], 40)
