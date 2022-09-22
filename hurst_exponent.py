be37b0f3d7919a6686627d0b9fc779a741b40f9e
David Woroniuk <david.woroniuk@gmail.com>
2022-09-22 18:02:57 +0100

    Modified Hurst Exponent

--- hurst_exponent.py
+++ hurst_exponent.py
@@ -1,25 +1,22 @@
 import pandas as pd
-from numpy.random import randn
-from numpy import cumsum, log, polyfit, sqrt, std, subtract
+from numpy import log, polyfit, sqrt, std, subtract
 
 
-def hurst_exponent(data_series):
-  '''
-  This function calculates the hurst exponent.
-  
-  Inputs:   data_series: the Pandas Series to be tested (Series).
+def hurst_exponent(data_series: pd.Series, verbose: bool = True):
+    """
+    This function calculates the hurst exponent. The function firstly checks that the input arguments are of the
+    correct type, followed by calculating the tau and poly values for the data_series, after which the outcome is
+    printed as the hurst exponent.
 
-  Outputs:  N/A.
-  '''
-  if isinstance(data_series, pd.DataFrame) is False:
-    raise TypeError("data_series argument must be a Pandas Series, data[['Series']].")
-    
-  lags = range(2, 100)
-  tau = [sqrt(std(subtract(data_series[lag:], data_series[:-lag]))) for lag in lags]
-  poly = polyfit(log(lags), log(tau), 1)
+    :param data_series: the series of data to be analysed. (pd.Series)
+    :param verbose: determines if the status is printed. (bool)
+    """
+    if not isinstance(data_series, pd.DataFrame):
+        raise TypeError("The data_series argument must be a Pandas Series.")
 
-  print('Hurst Exponent of {} is {}'.format(data_series.columns[0], (poly[0]*2.0)))
+    lags = range(2, 100)
+    tau = [sqrt(std(subtract(data_series[lag:], data_series[:-lag]))) for lag in lags]
+    poly = polyfit(log(lags), log(tau), 1)
+    if verbose:
+        print('Hurst Exponent of {} is {}'.format(data_series.columns[0], (poly[0] * 2.0)))
 
-
-
-hurst_exponent(dataset[['Open']])
